40974faf664e41aeebf7e90717040e38ec4085cc
简 <2447746158@qq.com>
2023-09-20 15:36:49 +0800

    minimal kernel

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,18 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"HELLO JIAN_OS";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vga = 0xb8000 as *mut u8;
+
+    for (i, &byte) in (0..).zip(HELLO.iter()) {
+        unsafe {
+            *vga.offset(i * 2) = byte;
+            *vga.offset(i * 2 + 1) = [0xe, 0xa, 0xb, 0xd][i as usize % 4];
+        }
+    }
+
+    loop {};
 }
